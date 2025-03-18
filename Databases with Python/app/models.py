fc31bb9aafa82d369fba61cd0bac326d0af9e401
shahriar0999 <shahriar0999@proton.me>
2025-03-18 11:27:02 +0600

    configure database table

--- Databases with Python/app/models.py
+++ Databases with Python/app/models.py
@@ -1,12 +1,13 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, func, TIMESTAMP
 
 
 class Post(Base):
-    __tablename__ = "Mpost"
+    __tablename__ = "Mpost1"
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=False)
+    published = Column(Boolean, server_default='TRUE',nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
 
 
