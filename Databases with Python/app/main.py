6b93f4c08158c8d5ab812d11adb77f448d20fe5b
shahriar0999 <shahriar0999@proton.me>
2025-03-22 15:11:16 +0600

    get-posts&create post using sqlalchemy

--- Databases with Python/app/main.py
+++ Databases with Python/app/main.py
@@ -20,7 +20,6 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    ratings: float = None
 
 def find_post(id):
     for post in my_post:
@@ -33,18 +32,18 @@ def find_post_index(id):
             return i
 
 # connect with database
-while True:
-    try:
-        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='1212759382',
-                          cursor_factory=RealDictCursor)
-        cursor =conn.cursor()
-        print("Successfully Connected to PostgreSQL")
-        break
-
-    except Exception as error :
-        print("could not connect to PostgreSQL")
-        print("error: ", error)
-        time.sleep(2)
+# while True:
+#     try:
+#         conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='1212759382',
+#                           cursor_factory=RealDictCursor)
+#         cursor =conn.cursor()
+#         print("Successfully Connected to PostgreSQL")
+#         break
+#
+#     except Exception as error :
+#         print("could not connect to PostgreSQL")
+#         print("error: ", error)
+#         time.sleep(2)
 
 my_post = [{'title': "hello world", 'content': 'many people are starting their coding journey by write Hello World program', 'id': 1},
            {'title': 'AI Change industry', 'content': 'due to AI involvment some jobs go down', 'id': 2}]
@@ -62,18 +61,32 @@ def test_sqlalchemy(db: Session = Depends(get_db)):
 
 
 @app.get("/posts")
-def get_post():
-    cursor.execute("""SELECT * FROM posts""")
-    posts =  cursor.fetchall()
+def get_post(db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts""")
+    # posts =  cursor.fetchall()
+    posts = db.query(models.Post).all()
     return {'message': posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def createPost(post: Post):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s)  RETURNING *""",
-                   (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
+def createPost(post: Post, db: Session = Depends(get_db)):
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s)  RETURNING *""",
+    #                (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+
+    # this line is of code inefficient cause when we have so many columns then it takes lots of time
+    # new_post = models.Post(title=post.title, content=post.content, published=post.published)
+
+    # easy way to do that
+    new_post = models.Post(
+        **post.dict())
+    # add that new post into database
+    db.add(new_post)
+    # push this post into database
+    db.commit()
+    # and display that crated post
+    db.refresh(new_post)
     return {'Message': new_post}
 
 
