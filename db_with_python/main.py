5afcbbfdc98068d46a13ae29d60a35008bfc8a7e
shahriar0999 <shahriar0999@proton.me>
2025-10-11 20:29:12 +0600

    delete a specific chat

--- db_with_python/main.py
+++ db_with_python/main.py
@@ -77,6 +77,19 @@ def get_post(id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} was not found")
     return ("chat", post)
+
+# delete a specific chat
+@app.delete("/chats/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    cursor.execute("""DELETE FROM chats WHERE id = %s RETURNING *""", (id,))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+
+    if deleted_post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"chat with id: {id} does not exist")
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
   
 
 if __name__ == "__main__":
