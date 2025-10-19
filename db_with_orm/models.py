ee3efe6c094befe10dd98b5f464359aa0861dd98
shahriar0999 <shahriar0999@proton.me>
2025-10-19 12:16:11 +0600

    create users table

--- db_with_orm/models.py
+++ db_with_orm/models.py
@@ -11,3 +11,11 @@ class Chat(Base):
     query = Column(String, nullable=False)
     response = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text(("now()")))
+
+class User(Base):
+    __tablename__ = "users_orm"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text(("now()")))
