4f4916fcda1672060cd78ff8a5d91d3de73dfc60
shahriar0999 <shahriar0999@proton.me>
2025-10-21 07:09:33 +0600

    created chat router path

---
+++ db_with_orm/routers/chats.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI, status, HTTPException, Response, Depends, APIRouter
+from typing import List
+from db_with_orm.database import get_db, engine
+from sqlalchemy.orm import Session
+from db_with_orm import models, schemas
+
+
+router = APIRouter(
+    prefix="/chats",
+    tags=["Chats"]
+)
+
+
+@router.get("/", response_model=List[schemas.PostBase])
+def get_posts(db: Session = Depends(get_db)):
+    chats = db.query(models.Chat).all()
+    return chats
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostBase)
+def create_chat(post: schemas.PostCreate, db: Session = Depends(get_db)):
+    new_chat = models.Chat(query=post.query, response=post.response)
+    db.add(new_chat)
+    db.commit()
+    db.refresh(new_chat)
+    return new_chat
+
+# get a specific chat
+@router.get("/{id}", response_model=schemas.PostBase)
+def get_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Chat).filter(models.Chat.id == id).first()
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found")
+    return post
+
+# delete a specific chat
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Chat).filter(models.Chat.id == id)
+    if post.first() == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"chat with id: {id} does not exist")
+    post.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+# update a existing chat
+@router.put("/{id}", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.PostBase)
+def update_chat(id: int, updated_post: Post, db: Session = Depends(get_db)):
+    post_query = db.query(models.Chat).filter(models.Chat.id == id)
+    post = post_query.first()
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"chat with id: {id} does not exist")
+    post_query.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
+    return post_query.first()
