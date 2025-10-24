2013ffce30f9716d79077137df6c9bf8dde7a7b2
shahriar0999 <shahriar0999@proton.me>
2025-10-24 12:24:40 +0600

    add response model

--- db_with_orm/routers/auth.py
+++ db_with_orm/routers/auth.py
@@ -5,7 +5,7 @@ import database , models, schemas, utils, oauth2
 
 router = APIRouter(tags=["Authentication"])
 
-@router.post("/login", status_code=status.HTTP_200_OK)
+@router.post("/login", status_code=status.HTTP_200_OK, response_model=schemas.Token)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
