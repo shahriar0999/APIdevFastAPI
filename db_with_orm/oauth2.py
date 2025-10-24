e8038f4b4e54639eff768f9b40d11e85a92ff0b5
shahriar0999 <shahriar0999@proton.me>
2025-10-24 12:26:31 +0600

    verify access token

--- db_with_orm/oauth2.py
+++ db_with_orm/oauth2.py
@@ -1,7 +1,11 @@
 from datetime import datetime, timedelta, timezone
+from fastapi import status, HTTPException, Response, Depends
+from fastapi.security.oauth2 import OAuth2PasswordBearer
 import jwt
 from jwt.exceptions import InvalidTokenError
+import schemas
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
@@ -14,3 +18,22 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encode_jwt
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData
+    except InvalidTokenError:
+        raise credentials_exception
+    return token_data
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    return verify_access_token(token, credentials_exception)
