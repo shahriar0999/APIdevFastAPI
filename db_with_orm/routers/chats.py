0cf2ec5c20e79b0fbcaaafc6047ed92b128fcd2f
shahriar0999 <shahriar0999@proton.me>
2025-10-24 12:25:53 +0600

    secure each path **before use those path user must need to login

--- db_with_orm/routers/chats.py
+++ db_with_orm/routers/chats.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, status, HTTPException, Response, Depends, APIRouter
 from typing import List
 import database
 from sqlalchemy.orm import Session
-import models, schemas
+import models, schemas, oauth2
 
 
 router = APIRouter(
@@ -18,7 +18,7 @@ def get_posts(db: Session = Depends(database.get_db)):
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostBase)
-def create_chat(post: schemas.PostCreate, db: Session = Depends(database.get_db)):
+def create_chat(post: schemas.PostCreate, db: Session = Depends(database.get_db), user_id: int = Depends(oauth2.get_current_user)):
     new_chat = models.Chat(query=post.query, response=post.response)
     db.add(new_chat)
     db.commit()
@@ -27,7 +27,7 @@ def create_chat(post: schemas.PostCreate, db: Session = Depends(database.get_db)
 
 # get a specific chat
 @router.get("/{id}", response_model=schemas.PostBase)
-def get_post(id: int, db: Session = Depends(database.get_db)):
+def get_post(id: int, db: Session = Depends(database.get_db), user_id: int = Depends(oauth2.get_current_user)):
     post = db.query(models.Chat).filter(models.Chat.id == id).first()
 
     if not post:
@@ -37,7 +37,7 @@ def get_post(id: int, db: Session = Depends(database.get_db)):
 
 # delete a specific chat
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(database.get_db)):
+def delete_post(id: int, db: Session = Depends(database.get_db), user_id: int = Depends(oauth2.get_current_user)):
     post = db.query(models.Chat).filter(models.Chat.id == id)
     if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -48,7 +48,7 @@ def delete_post(id: int, db: Session = Depends(database.get_db)):
 
 # update a existing chat
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.PostBase)
-def update_chat(id: int, updated_post: schemas.PostCreate, db: Session = Depends(database.get_db)):
+def update_chat(id: int, updated_post: schemas.PostCreate, db: Session = Depends(database.get_db), user_id: int = Depends(oauth2.get_current_user)):
     post_query = db.query(models.Chat).filter(models.Chat.id == id)
     post = post_query.first()
     if post == None:
