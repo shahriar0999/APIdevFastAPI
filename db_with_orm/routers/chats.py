0c5552afd93bf428c7c7443364d8bad2ff7b01fe
shahriar0999 <shahriar0999@proton.me>
2025-10-30 14:01:55 +0600

    credentials added for each path

--- db_with_orm/routers/chats.py
+++ db_with_orm/routers/chats.py
@@ -11,23 +11,23 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=List[schemas.PostBase])
-def get_posts(db: Session = Depends(database.get_db)):
-    chats = db.query(models.Chat).all()
+@router.get("/", response_model=List[schemas.Post])
+def get_posts(db: Session = Depends(database.get_db), current_user : int = Depends(oauth2.get_current_user)):
+    chats = db.query(models.Chat).filter(models.Chat.owner_id == current_user.id).all()
     return chats
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostBase)
-def create_chat(post: schemas.PostCreate, db: Session = Depends(database.get_db), user_id: int = Depends(oauth2.get_current_user)):
-    new_chat = models.Chat(query=post.query, response=post.response)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_chat(post: schemas.PostCreate, db: Session = Depends(database.get_db), current_user : int = Depends(oauth2.get_current_user),):
+    new_chat = models.Chat(owner_id=current_user.id, **post.dict())
     db.add(new_chat)
     db.commit()
     db.refresh(new_chat)
     return new_chat
 
 # get a specific chat
-@router.get("/{id}", response_model=schemas.PostBase)
-def get_post(id: int, db: Session = Depends(database.get_db), user_id: int = Depends(oauth2.get_current_user)):
+@router.get("/{id}", response_model=schemas.Post)
+def get_post(id: int, db: Session = Depends(database.get_db), current_user : int = Depends(oauth2.get_current_user)):
     post = db.query(models.Chat).filter(models.Chat.id == id).first()
 
     if not post:
@@ -37,23 +37,32 @@ def get_post(id: int, db: Session = Depends(database.get_db), user_id: int = Dep
 
 # delete a specific chat
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(database.get_db), user_id: int = Depends(oauth2.get_current_user)):
-    post = db.query(models.Chat).filter(models.Chat.id == id)
-    if post.first() == None:
+def delete_post(id: int, db: Session = Depends(database.get_db), current_user : int = Depends(oauth2.get_current_user)):
+    post_query = db.query(models.Chat).filter(models.Chat.id == id)
+    post = post_query.first()
+
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"chat with id: {id} does not exist")
-    post.delete(synchronize_session=False)
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Not authorized to perform requested action")
+
+    post_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 # update a existing chat
-@router.put("/{id}", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.PostBase)
-def update_chat(id: int, updated_post: schemas.PostCreate, db: Session = Depends(database.get_db), user_id: int = Depends(oauth2.get_current_user)):
+@router.put("/{id}", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.Post)
+def update_chat(id: int, updated_post: schemas.PostCreate, db: Session = Depends(database.get_db), current_user : int = Depends(oauth2.get_current_user)):
     post_query = db.query(models.Chat).filter(models.Chat.id == id)
     post = post_query.first()
     if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"chat with id: {id} does not exist")
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Not authorized to perform requested action")
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
     return post_query.first()
