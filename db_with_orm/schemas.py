3cbd727964d4911637af1e11da6a2f6fa368cf6c
shahriar0999 <shahriar0999@proton.me>
2025-11-03 20:42:48 +0600

    class order changes

--- db_with_orm/schemas.py
+++ db_with_orm/schemas.py
@@ -9,10 +9,20 @@ class PostBase(BaseModel):
 class PostCreate(PostBase):
     pass
 
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
 class Post(PostBase):
     id: int
     created_at: datetime
     owner_id: int
+    owner: UserOut
 
     class Config:
         orm_mode = True 
@@ -21,13 +31,6 @@ class UserCreate(BaseModel):
     email: EmailStr
     password: str
 
-class UserOut(BaseModel):
-    id: int
-    email: EmailStr
-    created_at: datetime
-
-    class Config:
-        orm_mode = True
 
 class UserLogin(BaseModel):
     email: EmailStr
