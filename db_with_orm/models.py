d2813dfdebef4c46ddb0d1b1d571848f94e2c895
shahriar0999 <shahriar0999@proton.me>
2025-11-03 20:41:07 +0600

    relations between two table using sql.orm relation

--- db_with_orm/models.py
+++ db_with_orm/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 
@@ -12,6 +13,7 @@ class Chat(Base):
     response = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text(("now()")))
     owner_id = Column(Integer, ForeignKey("users_orm.id", ondelete="CASCADE"), nullable=False)
+    owner = relationship("User")  # Establish relationship with User model
 
 class User(Base):
     __tablename__ = "users_orm"
