ab431856772b789b38cc020fcec48babf600716e
shahriar0999 <shahriar0999@proton.me>
2025-11-12 10:11:16 +0600

    schemas for vote route

--- db_with_orm/schemas.py
+++ db_with_orm/schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from typing import Optional
+from pydantic.types import conint
 
 class PostBase(BaseModel):
     query: str
@@ -42,3 +43,7 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[int] = None
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1)
