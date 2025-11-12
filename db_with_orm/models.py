41c4b8678895bebe07f693d4c36e2abee5f90840
shahriar0999 <shahriar0999@proton.me>
2025-11-12 10:10:51 +0600

    create a vote table

--- db_with_orm/models.py
+++ db_with_orm/models.py
@@ -22,3 +22,9 @@ class User(Base):
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text(("now()")))
+
+class Vote(Base):
+    __tablename__ = "vote"
+    
+    user_id = Column(Integer, ForeignKey("users_orm.id", ondelete='CASCADE'), primary_key=True)
+    post_id = Column(Integer, ForeignKey("chats_orm.id", ondelete='CASCADE'), primary_key=True)
