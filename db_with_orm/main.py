5093fa0feb2caf13a3e050bc1ca76c4fa7e4bf2a
shahriar0999 <shahriar0999@proton.me>
2025-11-26 04:22:36 +0600

    load credentials from .env

--- db_with_orm/main.py
+++ db_with_orm/main.py
@@ -3,25 +3,14 @@ import uvicorn
 from fastapi import FastAPI, status, HTTPException, Response, Depends
 from langchain_groq import ChatGroq
 from langchain.schema import HumanMessage
-from dotenv import load_dotenv
 from database import get_db, engine
 from sqlalchemy.orm import Session
-import models, schemas, utils
+import models, schemas, utils, config
 from routers import chats, users, auth, vote
 
 
 
 models.Base.metadata.create_all(bind=engine)
-
-# Load API key from .env
-load_dotenv()
-groq_api_key = os.getenv("GROQ_API_KEY")
-
-# Initialize Groq chat model
-chat = ChatGroq(
-    api_key=groq_api_key,
-    model="openai/gpt-oss-20b"  # or any available Groq model
-)
 
 
 app = FastAPI()
