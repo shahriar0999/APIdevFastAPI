08aeb34be653f065be695647c446cebf664b4794
shahriar0999 <shahriar0999@proton.me>
2025-11-30 09:28:22 +0600

    add foregin key to posts table

--- db_with_orm/alembic/versions/0105aacf5853_add_foregin_key_to_posts_table.py
+++ db_with_orm/alembic/versions/0105aacf5853_add_foregin_key_to_posts_table.py
@@ -19,10 +19,13 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    """Upgrade schema."""
+    op.add_column('posts', sa.Column('owner_id', sa.Integer(), nullable=False))
+    op.create_foreign_key('post_users_fk', source_table="posts", referent_table="users_orm", local_cols=[
+                          'owner_id'], remote_cols=['id'], ondelete="CASCADE")
     pass
 
 
 def downgrade() -> None:
-    """Downgrade schema."""
+    op.drop_constraint('post_users_fk', table_name='posts')
+    op.drop_column('posts', 'owner_id')
     pass
